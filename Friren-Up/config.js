84021bffb0c2496ab5907eb52fe60d9eb6464cc6
leoxito <erenxz01@gmail.com>
2026-01-07 22:36:27 +0000

    Revertir a global en config.js

--- Friren-Up/config.js
+++ Friren-Up/config.js
@@ -7,26 +7,26 @@ import fs from "fs"
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
 // global.botNumber = SIN NUMERO //Ejemplo: 513218138672
 
-globalThis.owner = ['51918755472'/*,'owner 2', 'owner 3']*/
-globalThis.libreria = "Baileys Multi Device-MD"
-globalThis.vs = "^NewUpdate | V1"
-globalThis.sessions = "Sessions"
-globalThis.botname = "❀ 𝐅𝐫𝐢𝐞𝐫𝐞𝐧-𝐁𝐨𝐭"
-globalThis.textbot = "✿ 𝗙𝗿𝗶𝗲𝗿𝗲𝗻-𝗠𝗗 𝘅 𝗹𝗲𝗼𝘅𝗶𝘁𝗼𝗗𝗲𝘃.𝘅𝘆𝘇"
-globalThis.dev = "✐ 𝐏𝐨𝐰𝐨𝐫𝐞𝐝 𝐛𝐲 𝐥𝐞𝐨𝐱𝐢𝐭𝐨𝐃𝐞𝐯.𝐱𝐲𝐳"
-globalThis.author = "❀ 𝙀𝙘𝙝𝙤 𝙮 𝘿𝙚𝙫𝙡𝙤𝙥𝙖𝙙𝙤 𝙥𝙤𝙧 𝙡𝙚𝙤𝙭𝙞𝙩𝙤𝘿𝙚𝙫.𝙭𝙮𝙯"
-globalThis.etiqueta = "leoxitoDev.xyz"
-globalThis.currency = "¥enes"
-globalThis.packname = ``
-globalThis.author = ''
-globalThis.group = "https://chat.whatsapp.com/DTy5WmWeyHp4oOzFlydUhN"
-globalThis.community = "https://chat.whatsapp.com/L3Kruk5HQCX0SHe4nzPOGX"
-globalThis.channel = "https://whatsapp.com/channel/0029VbBvZH5LNSa4ovSSbQ2N"
-globalThis.github = "https://github.com/leoxito/FrirenNews-MD"
-globalThis.gmail = "leo.xzy01@gmail.com"
-globalThis.multiplier = 69 
-globalThis.maxwarn = '2'
-globalThis.ch = {
+global.owner = ['51918755472'/*,'owner 2', 'owner 3']*/
+global.libreria = "Baileys Multi Device-MD"
+global.vs = "^NewUpdate | V1"
+global.sessions = "Sessions"
+global.botname = "❀ 𝐅𝐫𝐢𝐞𝐫𝐞𝐧-𝐁𝐨𝐭"
+global.textbot = "✿ 𝗙𝗿𝗶𝗲𝗿𝗲𝗻-𝗠𝗗 𝘅 𝗹𝗲𝗼𝘅𝗶𝘁𝗼𝗗𝗲𝘃.𝘅𝘆𝘇"
+global.dev = "✐ 𝐏𝐨𝐰𝐨𝐫𝐞𝐝 𝐛𝐲 𝐥𝐞𝐨𝐱𝐢𝐭𝐨𝐃𝐞𝐯.𝐱𝐲𝐳"
+global.author = "❀ 𝙀𝙘𝙝𝙤 𝙮 𝘿𝙚𝙫𝙡𝙤𝙥𝙖𝙙𝙤 𝙥𝙤𝙧 𝙡𝙚𝙤𝙭𝙞𝙩𝙤𝘿𝙚𝙫.𝙭𝙮𝙯"
+global.etiqueta = "leoxitoDev.xyz"
+global.currency = "¥enes"
+global.packname = ``
+global.author = ''
+global.group = "https://chat.whatsapp.com/DTy5WmWeyHp4oOzFlydUhN"
+global.community = "https://chat.whatsapp.com/L3Kruk5HQCX0SHe4nzPOGX"
+global.channel = "https://whatsapp.com/channel/0029VbBvZH5LNSa4ovSSbQ2N"
+global.github = "https://github.com/leoxito/FrirenNews-MD"
+global.gmail = "leo.xzy01@gmail.com"
+global.multiplier = 69 
+global.maxwarn = '2'
+global.ch = {
 ch1: "120363404434164076@newsletter"
 }
 
