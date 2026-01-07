a785314d1443fbd59f7ebe5ebb5cdd6d36fb3550
leoxito <erenxz01@gmail.com>
2026-01-07 14:14:32 -0400

    ^𝗡𝗲𝘄𝘀-𝗨𝗽 🌿

---
+++ config.js
@@ -0,0 +1,26 @@
+import { watchFile, unwatchFile } from 'fs' 
+import chalk from 'chalk'
+import { fileURLToPath } from 'url'
+
+global.owner = [ ['', '-', true], [''] ]
+global.mods = []
+global.prems = []
+   
+
+global.packname = ``
+global.author = ''
+global.botname = 'Sylpha - Bot'
+
+global.name_canal = '-'
+global.id_canal = '120363274577422945@newsletter'
+global.canal = 'https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n'
+
+global.multiplier = 69 
+global.maxwarn = '2'
+
+let file = fileURLToPath(import.meta.url)
+watchFile(file, () => {
+  unwatchFile(file)
+  console.log(chalk.redBright("Update 'config.js'"))
+  import(`${file}?update=${Date.now()}`)
+})
