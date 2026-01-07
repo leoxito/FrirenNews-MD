0e4083d81db14ae2457de92f4d19906c00d904d9
﹫ᥣᥱ᥆.хყz 🕷 <erenxz01@gmail.com>
2026-01-07 17:52:28 -0400

    Actualizar config.js

--- Friren-Up/config.js
+++ Friren-Up/config.js
@@ -1,26 +1,38 @@
-import { watchFile, unwatchFile } from 'fs' 
-import chalk from 'chalk'
-import { fileURLToPath } from 'url'
+import { watchFile, unwatchFile } from "fs"
+import chalk from "chalk"
+import { fileURLToPath } from "url"
+import fs from "fs"
 
-global.owner = [ ['', '-', true], [''] ]
-global.mods = []
+
+//INFO: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
+//Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
+// global.botNumber = SIN NUMERO //Ejemplo: 513218138672
+
+global.owner = ['51900373696','595974154768', '51921826291']
+global.suittag = [] 
 global.prems = []
-   
+global.libreria = "Baileys Multi Device"
+global.vs = "^1.8.2|Latest"
+global.sessions = "Sessions/Principal"
+global.botname = "Oguri-Cap"
+global.textbot = "Oguri, made with love by MELODIA"
+global.dev = "© Powered by MELODIA"
+global.author = "© Made with love by MELODIA"
+global.etiqueta = "MELODIA"
+global.currency = "¥enes"
+global.group = "https://chat.whatsapp.com/EYi0JuSqDj3LYJ83ohRdMm"
+global.community = "https://chat.whatsapp.com/HY3r3RwkOOKCs6OxCzsEFW"
+global.channel = "https://whatsapp.com/channel/0029VbBZ4YX4inoqvA74nA20"
+global.github = "https://github.com/melodiabl/OguriCap-Bot.git"
+global.gmail = "melodiayaoivv@gmail.com"
+global.ch = {
+ch1: "120363404287449613@newsletter"
+}
 
-global.packname = ``
-global.author = ''
-global.botname = 'Sylpha - Bot'
-
-global.name_canal = '-'
-global.id_canal = '120363274577422945@newsletter'
-global.canal = 'https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n'
-
-global.multiplier = 69 
-global.maxwarn = '2'
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
-  unwatchFile(file)
-  console.log(chalk.redBright("Update 'config.js'"))
-  import(`${file}?update=${Date.now()}`)
+unwatchFile(file)
+console.log(chalk.redBright("Update 'settings.js'"))
+import(`${file}?update=${Date.now()}`)
 })
