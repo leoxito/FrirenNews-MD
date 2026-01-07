4baa50c71e4290c9980262ee1221bab03a8a64ec
﹫ᥣᥱ᥆.хყz 🕷 <erenxz01@gmail.com>
2026-01-07 18:40:45 -0400

    Actualizar main.js

--- Friren-Up/main.js
+++ Friren-Up/main.js
@@ -1,7 +1,6 @@
 const global = globalThis;
-
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1'
-import '../config.js'
+import './config.js'
 import { createRequire } from 'module'
 import path, { join } from 'path'
 import {fileURLToPath, pathToFileURL} from 'url'
