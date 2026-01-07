167e0a00223876fc458af32ad2cd5235e3605987
leoxito <erenxz01@gmail.com>
2026-01-07 22:36:06 +0000

    Resolver conflicto final y usar globalThis

--- Friren-Up/config.js
+++ Friren-Up/config.js
@@ -3,27 +3,9 @@ import chalk from "chalk"
 import { fileURLToPath } from "url"
 import fs from "fs"
 
-<<<<<<< HEAD
-globalThisThis.owner = [ ['', '-', true], [''] ]
-globalThisThis.mods = []
-globalThisThis.prems = []
-   
-
-globalThisThis.packname = ``
-globalThisThis.author = ''
-globalThisThis.botname = 'Sylpha - Bot'
-
-globalThisThis.name_canal = '-'
-globalThisThis.id_canal = '120363274577422945@newsletter'
-globalThisThis.canal = 'https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n'
-
-globalThisThis.multiplier = 69 
-globalThisThis.maxwarn = '2'
-=======
-
 //INFO: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
-// globalThis.botNumber = SIN NUMERO //Ejemplo: 513218138672
+// global.botNumber = SIN NUMERO //Ejemplo: 513218138672
 
 globalThis.owner = ['51918755472'/*,'owner 2', 'owner 3']*/
 globalThis.libreria = "Baileys Multi Device-MD"
@@ -47,8 +29,6 @@ globalThis.maxwarn = '2'
 globalThis.ch = {
 ch1: "120363404434164076@newsletter"
 }
-
->>>>>>> ceeb16d2b660326c31a0e39c69805798eb19126d
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
