ced872c21d6495c4dd3adff9ce9425a0324322e1
leoxito <erenxz01@gmail.com>
2026-01-07 22:35:44 +0000

    Cambiar global por globalThis en config.js

--- Friren-Up/config.js
+++ Friren-Up/config.js
@@ -2,21 +2,21 @@ import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
-global.owner = [ ['', '-', true], [''] ]
-global.mods = []
-global.prems = []
+globalThis.owner = [ ['', '-', true], [''] ]
+globalThis.mods = []
+globalThis.prems = []
    
 
-global.packname = ``
-global.author = ''
-global.botname = 'Sylpha - Bot'
+globalThis.packname = ``
+globalThis.author = ''
+globalThis.botname = 'Sylpha - Bot'
 
-global.name_canal = '-'
-global.id_canal = '120363274577422945@newsletter'
-global.canal = 'https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n'
+globalThis.name_canal = '-'
+globalThis.id_canal = '120363274577422945@newsletter'
+globalThis.canal = 'https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n'
 
-global.multiplier = 69 
-global.maxwarn = '2'
+globalThis.multiplier = 69 
+globalThis.maxwarn = '2'
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
