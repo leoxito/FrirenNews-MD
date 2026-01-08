4d900133a5179328f5aca17a3141a64817fc29c2
﹫ᥣᥱ᥆.хყz 🕷 <erenxz01@gmail.com>
2026-01-08 16:40:52 -0400

    Actualizar downloader-Tiktok.js

--- plugins/downloader-Tiktok.js
+++ plugins/downloader-Tiktok.js
@@ -1,7 +1,7 @@
 import { getDownloadLink } from '../Scrapers/Tiktokdl.js';
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `> ❀ *Pasa el link de TikTok…*\n> *https://vm.tiktok.com/××××××*`
+    if (!args[0]) throw `> ❀ *Pasa el link de TikTok…*\n*https://vm.tiktok.com/××××××*`
 
     await m.react('🕐')
     try {
