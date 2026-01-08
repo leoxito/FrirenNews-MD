f6b14536294ab2c20ccac1b56b20773fdfec4016
﹫ᥣᥱ᥆.хყz 🕷 <erenxz01@gmail.com>
2026-01-08 17:12:50 -0400

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -6,7 +6,7 @@ import { xpRange } from '../lib/levelling.js'
 let tags = {
   'main': '𝐈𝐍𝐅𝐎',
   'search': '𝐒𝐄𝐀𝐑𝐂𝐇',
-  'dl': '𝐃𝐄𝐒𝐂𝐀𝐑𝐆𝐀𝐒',
+  'downloader': '𝐃𝐄𝐒𝐂𝐀𝐑𝐆𝐀𝐒',
   'tools': '𝐓𝐎𝐎𝐋𝐒',
   'sticker': '𝐒𝐓𝐈𝐂𝐊𝐄𝐑𝐒',
   'owner': '𝐂𝐑𝐄𝐀𝐃𝐎𝐑',
