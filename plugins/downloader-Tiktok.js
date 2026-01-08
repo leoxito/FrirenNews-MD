3f0342fdb81c5652117bb85a5c8863a403ff0bbf
﹫ᥣᥱ᥆.хყz 🕷 <erenxz01@gmail.com>
2026-01-08 16:39:12 -0400

    Actualizar downloader-Tiktok.js

--- plugins/downloader-Tiktok.js
+++ plugins/downloader-Tiktok.js
@@ -1,31 +1,27 @@
 import { getDownloadLink } from '../Scrapers/Tiktokdl.js';
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `> ❀ *Pasa el link de TikTok…*\n> *Ejemplo: ${usedPrefix + command} https://vm.tiktok.com/xyz*`
+    if (!args[0]) throw `> ❀ *Pasa el link de TikTok…*\n> *https://vm.tiktok.com/××××××*`
 
     await m.react('🕐')
     try {
-        // Ejecutamos la función que está en la ruta Scrapers/Tiktokdl.js
         const links = await getDownloadLink(args[0]);
+        if (!links || links.length === 0) throw 'Error';
 
-        if (links.length === 0) throw 'No se encontraron enlaces de descarga.';
-
-        // Enviamos el video obtenido por el scraper
         await conn.sendMessage(m.chat, { 
             video: { url: links[0] }, 
             caption: '*Aquí tienes tu video de TikTok ฅ^•ﻌ•^ฅ*' 
         }, { quoted: m });
-
+        
         await m.react('✅')
     } catch (e) {
-        console.error(e)
         await m.react('❌')
-        m.reply('> ❀ *Ocurrió un error al descargar el video. Inténtalo de nuevo más tarde.*')
+        m.reply('> ❀ *Ocurrió un error al descargar el video.*')
     }
 }
 
-handler.help = ['tiktok «link»']
+handler.help = ['tiktok']
 handler.tags = ['downloader']
-handler.command = ['tt', 'tiktok', 'tiktokdl']
+handler.command = ['tt', 'tiktok']
 
 export default handler
