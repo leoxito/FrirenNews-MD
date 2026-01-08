718352426c90e29bd6883553039021a9ba0d3034
﹫ᥣᥱ᥆.хყz 🕷 <erenxz01@gmail.com>
2026-01-08 17:09:58 -0400

    Actualizar owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -8,7 +8,7 @@ await m.react('✅')
 }}
 handler.help = ['update']
 handler.tags = ['owner']
-handler.command = ['update', 'actualizar', 'fix', 'fixed'] 
+handler.command = ['update', 'actualizar', 'up', 'fixed'] 
 handler.rowner = true
 
 export default handler
