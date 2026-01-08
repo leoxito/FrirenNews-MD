47cc502cd3564c967f557712a2ad4f6f6b11244e
﹫ᥣᥱ᥆.хყz 🕷 <erenxz01@gmail.com>
2026-01-08 14:20:07 -0400

    Actualizar handler.js

--- Friren-Up/handler.js
+++ Friren-Up/handler.js
@@ -161,7 +161,12 @@ export async function handler(chatUpdate) {
         const isAdmin = isRAdmin || user?.admin == 'admin' || false
         const isBotAdmin = bot?.admin || false
 
-        const ___dirname = path.join(path.dirname(fileURLToPath(import.meta.url)), '../plugins./Comandos')
+        // CORRECCIÓN: Apuntar a la carpeta correcta Comandos dentro de plugins
+        const ___dirname = path.join(path.dirname(fileURLToPath(import.meta.url)), '../plugins/Comandos')
+        
+        // Agrega esto para depuración
+        console.log('🔍 Buscando comandos en:', ___dirname)
+        
         for (let name in global.plugins) {
             let plugin = global.plugins[name]
             if (!plugin)
