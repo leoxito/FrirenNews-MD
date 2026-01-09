47482d295bf4b20ddd349a6d9ad9889c47bd610d
﹫ᥣᥱ᥆.хყz 🕷 <erenxz01@gmail.com>
2026-01-09 16:52:19 -0400

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -107,7 +107,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
 │✦ *Versión* : ${_package.version || '1.0.0'}
 │✦ *Uptime* : ${uptime}
 │✐ *RAM* : ${(process.memoryUsage().rss / 1024 / 1024).toFixed(2)} MB
-│❀ *${greeting}* 
+│ *${greeting}* 
 ╰ׅ┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
 
 `
