bfa804d9a4fcfad97175949f759ca9021a90a926
﹫ᥣᥱ᥆.хყz 🕷 <erenxz01@gmail.com>
2026-01-10 08:00:58 -0400

    Actualizar downloader-Instagram.js

--- plugins/downloader-Instagram.js
+++ plugins/downloader-Instagram.js
@@ -1,31 +1,24 @@
 import { getDownloadLink } from '../Scrapers/Instagramdl.js'
 
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-    if (!args[0]) throw `*❀ Ingrese el enlace de TikTok.*\n> Ejemplo: *${usedPrefix + command} https://vm.tiktok.com/xxxxxx*`);
+    if (!args[0]) throw `*❀ Ingrese el enlace de Instagram.*\n> Ejemplo: *${usedPrefix + command} https://www.instagram.com/reel/xxxxxx`
     
-    // Reacción de búsqueda 🕐
     await conn.sendMessage(m.chat, { react: { text: "🕐", key: m.key } })
     
     try {
         const data = await getDownloadLink(args[0])
         
-        if (data.error) throw data.message
-        if (!data.best || !data.best.url) throw '❌ No se pudo obtener el video'
+        if (data.error) throw data.error
+        if (!data.best || !data.best.url) throw '*❌ No se pudo obtener el video*'
         
-        const videoUrl = data.best.url
-        
-        // Enviar el video con caption simple
         await conn.sendMessage(m.chat, {
-            video: { url: videoUrl },
+            video: { url: data.best.url },
             caption: `> *☁️ Aqui Tienes Tu Video*`
         }, { quoted: m })
         
-        // Reacción de éxito ✅️
         await conn.sendMessage(m.chat, { react: { text: "✅️", key: m.key } })
         
     } catch (error) {
-        console.error(error)
-        // Reacción de error ❌️
         await conn.sendMessage(m.chat, { react: { text: "❌️", key: m.key } })
         await m.reply(`❌ *Error:* ${error}`)
     }
@@ -33,6 +26,8 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 
 handler.help = ['instagram']
 handler.tags = ['downloader']
-handler.command = ['instagram', 'ig', 'igdl']
+handler.command = ['instagram', 'ig', 'igdl', 'reel']
+handler.limit = true
+handler.premium = false
 
 export default handler
